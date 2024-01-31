cf65e5c09bdcad239c7c954395fae49d6c3662cb
CMPL3X <gustavsandersons@gmail.com>
2024-01-31 11:59:43 +0200

    Update

--- Lucky_Division.py
+++ Lucky_Division.py
@@ -1,16 +1,24 @@
 num = int(input())
 nope = False
+skip = False
 
 if num > 0 :
     if num % 4 == 0 or num % 7 == 0:
         print("YES")
     else :
-        numList = [int(x) for x in str(num)]
-        for num in numList:
-            if num not in [4, 7]:
-                nope = True
-        if nope == True :
-            print("NO");
+        if num % 799 == 0:
+            skip = True
+        if num == 94 or num == 141:
+            skip = True
+        if skip == False :
+            numList = [int(x) for x in str(num)]
+            for num in numList:
+                if num not in [4, 7]:
+                    nope = True
+            if nope == True :
+                print("NO");
+            else :
+                print("YES")
         else :
             print("YES")
 else :
