a104ab1e94d8cbeec4eff27ae73aeca280a8e909
CMPL3X <gustavsandersons@gmail.com>
2024-11-14 13:39:13 +0200

    Update

--- Helpful_Maths.py
+++ Helpful_Maths.py
@@ -1,2 +1,18 @@
-# input - string ar nesakārtotiem skaitļiem
-# output - tas pats, bet sakārtots no (0 ; +B)
+input = input()
+
+split = input.split("+")
+sorted = sorted(split)
+
+count = 1
+ino = True
+
+final = str(sorted[0])
+
+while ino == True :
+    if len(input) >= 2 :
+        final = str(final) + "+" + str(sorted[count])
+        count = count + 1
+    if int(count) >= int(len(split)) :
+        ino = False
+
+print(final)
